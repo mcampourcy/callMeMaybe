409bbb2ccf4ead53d05607af03b1de56326a4726
mimi <mireille.campourcy@gmail.com>
2017-02-14 21:38:38 +0100

    [VIEWS] Set up home template with datas

--- app/views/templates/homeView.php
+++ app/views/templates/homeView.php
@@ -1,1 +1,12 @@
-<p>Hello</p>
+<ul>
+    <?php
+    foreach ($homeData as $person){
+        $link = FO_URL.$person->getId();
+        ?>
+        <li>
+            <a href="<?=$link?>"><?=$person->getFirstname().' '.$person->getLastname()?></a>
+        </li>
+        <?php
+    }//endfor
+    ?>
+</ul>
